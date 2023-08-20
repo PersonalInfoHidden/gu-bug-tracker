af1cf4d766b1a72939cc50756a30498729f4ef7a
PersonalInfoHidden <106556443+PersonalInfoHidden@users.noreply.github.com>
2023-08-20 21:44:10 +0200

    added small team ui

--- src/app/team/team.tsx
+++ src/app/team/team.tsx
@@ -7,10 +7,19 @@ type Team = Database["public"]["Tables"]["Teams"]["Row"];
 
 function Team({ team }: { team: Team }) {
     return (
-        <Link href={`/team/${team.id}`} className="p-1 hover:border">
+        <Link
+            href={`/team/${team.id}`}
+            className="p-1 transition hover:scale-90"
+        >
             <div className="border p-4">
                 <h1 className="uppercase">{team.name}</h1>
-                {team.paid ? <></> : <h2>Free</h2>}
+                {team.paid ? (
+                    <></>
+                ) : (
+                    <h2 className="bg-accent bg-opacity-0 text-primary px-3 py-1 rounded-md font-bold">
+                        Free
+                    </h2>
+                )}
             </div>
         </Link>
     );
